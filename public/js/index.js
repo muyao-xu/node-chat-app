1cb5ecf7aa445ec4289930f390e23b41ab8de1c7
Muyao Xu <muyaoxu@usc.edu>
2018-06-26 15:20:22 -0700

    Add js file

---
+++ public/js/index.js
@@ -0,0 +1,20 @@
+var socket = io();	//loaded from the library
+
+socket.on('connect', function () {
+	console.log('Connected to server');
+
+	// socket.emit('createMessage', {
+	// 	from: 'Andrew',
+	// 	text: 'Hey. This is from Andrew'
+	// });
+});
+
+socket.on('disconnect', function () {
+	console.log('Disconnected from server');
+});
+
+
+socket.on('newMessage', function (message) {
+	console.log('New message', message);
+});
+
