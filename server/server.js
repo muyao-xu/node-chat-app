6fc0e0b1d95fd3750fa995a8ece344c426e428ba
Muyao Xu <muyaoxu@usc.edu>
2018-07-22 22:23:46 +0800

    Add room chat

--- server/server.js
+++ server/server.js
@@ -45,9 +45,12 @@ io.on('connection', (socket) => {
 
 
 	socket.on('createMessage', (message, callback) => {
-		console.log('createEmail', message);
-		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('This is from the server.');
+		var user = users.getUser(socket.id);
+
+		if (user && isRealString(message.text)) {
+			io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+		}
+		callback();
 		// ------broadcast
 		// socket.broadcast.emit('newMessage', {		
 		// 	from: message.from,
@@ -57,7 +60,11 @@ io.on('connection', (socket) => {
 	});
 
 	socket.on('createLocationMessage', (coords) => {
-		io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+		var user = users.getUser(socket.io);
+
+		if (user) {
+			io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+		}
 	});
 
 	socket.on('disconnect', () => {
